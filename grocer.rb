4ed24fa0db0aacc438ab9dc86cd4b67735e3db47
salubrious-commit-8954 <wangyy395@gmail.com>
2020-02-23 23:17:20 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -22,7 +22,8 @@ def consolidate_cart(cart)
     item = cart[counter][:item]
     ifSeen = find_item_by_name_in_collection(item, ans) 
     if ifSeen
-      
+      ifSeen[:count] += 1 
+      else 
       
 end
 
