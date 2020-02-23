b2dee4c8cb38dbd198ba63e660b8094c3243639a
salubrious-commit-8954 <wangyy395@gmail.com>
2020-02-23 23:14:11 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -15,7 +15,7 @@ def consolidate_cart(cart)
   #
   # REMEMBER: This returns a new Array that represents the cart. Don't merely
   # change `cart` (i.e. mutate) it. It's easier to return a new thing.
-  
+  ans = []
 end
 
 def apply_coupons(cart, coupons)
